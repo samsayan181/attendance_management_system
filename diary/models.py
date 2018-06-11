f2b844eca9ff55049ccea4e7d5df05b40f1215e9
Harsh Vardhan Sharma <harshavardhana.619@gmail.com>
2018-06-12 01:24:22 +0530

    Roll no now an inteeger field

--- diary/models.py
+++ diary/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
 	name = models.CharField(max_length=200)
-	roll_no = models.CharField(max_length=200, unique=True)
+	roll_no = models.PositiveIntegerField(unique=True)
 
 	class Meta:
-		ordering = ['-roll_no']
+		ordering = ['roll_no']
 
 	def __str__(self):
 		return "{} ({})".format(self.name, self.roll_no)
